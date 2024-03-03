77479e36ed558360c353e6209d1ca6d25120f342
Neeraj Kumar <nk35715945@gmail.com>
2024-03-03 15:30:56 +0530

    files are added

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,17 +1,68 @@
 const User = require('../models/userModel') ;
+const bcrypt = require('bcryptjs') ;
+const jwt = require('jsonwebtoken') ;
+const createError = require('../utils/appError') ;
 
 
-let singup = async (req,res,next)=>{
+
+ exports.signup = async (req,res,next)=>{
     try{
          const user = await User.findOne({email:req.body.email}) ;
          if(user){
-             return next()
+             return next( new createError('User already exists',400)) ;
          }
+         const hashedPassword  = await bcrypt.hash(req.body.password,12) ;
+         const newUser = await User.create({
+            ...req.body,
+            password:hashedPassword ,
+         })
+
+         // JWT token 
+         const token = jwt.sign({_id:newUser._id}, "secretkey123",{
+             expiresIn: '90d',
+         });
+         res.status(201).json({
+             status :"success",
+             message : "User registered successfully",
+             token,
+         }) ;
     }catch(error){}
 }
 
-let login = async (req,res,next)=>{
-     
+exports.login = async (req,res,next)=>{
+     try{
+        const {email,password} = req.body ;
+        const user = await User.findOne({email}) ;
+        
+        if(!user){
+             return  next( new createError("User not Found",404)) ;
+        }
+
+        const isPassword = await bcrypt.compare( password,user.password) ;
+        if(!isPassword){
+            return next(new createError("Incorrect email or Password"),401) ;
+        }
+
+        const token = jwt.sign({_id: user._id}, 'secretkey123',{
+            expiresIn:'90d',
+        })
+
+        res.status(200).json({
+            status:"successfully",
+            token,
+            message : 'Logged in Successfully',
+            user:{
+                _id: user._id,
+                name: user.name,
+                email:user.email ,
+                role:user.role,
+            }
+        });
+
+     }catch(error){
+        next(error)
+     }
 }
+
 
 
--- server/server.js
+++ server/server.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const mongo = require('mongoose') 
 const cors = require("cors")
+const authRouter = require('./routes/authRoute') 
 
-
-const app = express()
 const port = 3003
+const app = express()
+
+//  Middle wares
 
 app.use(express.json())
 app.use(cors())
 
+// Route
 
+app.use('/api/auth',authRouter) 
+
+// Mongo Db connection
 
 mongo.connect('mongodb://localhost:27017/authentication')
 .then(()=>{
@@ -18,9 +24,24 @@ mongo.connect('mongodb://localhost:27017/authentication')
    console.log('Connection error') ;
 })
 
+
+// global Error Handler 
+
+app.use((err,req,res,next) =>{
+   err.statusCode = err.statusCode || 500 ;
+   err.status = err.status || 'error' 
+
+   res.status(err.statusCode).json({
+       status :err.status,
+       message:err.message,
+   })
+})
+
 app.get('/', (req, res) => {
   res.send('I am running')
 })
+
+// Here, server will be running
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
